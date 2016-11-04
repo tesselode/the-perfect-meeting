090b1bb1f49d524cbbcd6733a37ffd41fe55c6a4
Andrew Minnich <aminnich3@gmail.com>
2016-11-03 23:16:48 -0400

    Update build script for LOVE 0.10.2

--- build.py
+++ build.py
@@ -38,38 +38,38 @@ os.rename('binaries/logo-bounce.zip', 'binaries/logo-bounce.love')
 # make win32 build
 print()
 print('Creating win32 build...')
-urllib.request.urlretrieve('https://bitbucket.org/rude/love/downloads/love-0.10.1-win32.zip', 'binaries/love-win32.zip')
+urllib.request.urlretrieve('https://bitbucket.org/rude/love/downloads/love-0.10.2-win32.zip', 'binaries/love-win32.zip')
 with zipfile.ZipFile('binaries/love-win32.zip', 'r') as z:
     z.extractall('binaries')
 os.remove('binaries/love-win32.zip')
-shutil.copyfile('binaries/logo-bounce.love', 'binaries/love-0.10.1-win32/logo-bounce.love')
-with cd('binaries/love-0.10.1-win32'):
+shutil.copyfile('binaries/logo-bounce.love', 'binaries/love-0.10.2-win32/logo-bounce.love')
+with cd('binaries/love-0.10.2-win32'):
     subprocess.call('copy /b love.exe+logo-bounce.love logo-bounce.exe', shell=True)
-os.remove('binaries/love-0.10.1-win32/changes.txt')
-os.remove('binaries/love-0.10.1-win32/game.ico')
-os.remove('binaries/love-0.10.1-win32/logo-bounce.love')
-os.remove('binaries/love-0.10.1-win32/love.exe')
-os.remove('binaries/love-0.10.1-win32/love.ico')
-os.remove('binaries/love-0.10.1-win32/readme.txt')
-os.rename('binaries/love-0.10.1-win32', 'binaries/logo-bounce-win32')
+os.remove('binaries/love-0.10.2-win32/changes.txt')
+os.remove('binaries/love-0.10.2-win32/game.ico')
+os.remove('binaries/love-0.10.2-win32/logo-bounce.love')
+os.remove('binaries/love-0.10.2-win32/love.exe')
+os.remove('binaries/love-0.10.2-win32/love.ico')
+os.remove('binaries/love-0.10.2-win32/readme.txt')
+os.rename('binaries/love-0.10.2-win32', 'binaries/logo-bounce-win32')
 
 # make win64 build
 print()
 print('Creating win64 build...')
-urllib.request.urlretrieve('https://bitbucket.org/rude/love/downloads/love-0.10.1-win64.zip', 'binaries/love-win64.zip')
+urllib.request.urlretrieve('https://bitbucket.org/rude/love/downloads/love-0.10.2-win64.zip', 'binaries/love-win64.zip')
 with zipfile.ZipFile('binaries/love-win64.zip', 'r') as z:
     z.extractall('binaries')
 os.remove('binaries/love-win64.zip')
-shutil.copyfile('binaries/logo-bounce.love', 'binaries/love-0.10.1-win64/logo-bounce.love')
-with cd('binaries/love-0.10.1-win64'):
+shutil.copyfile('binaries/logo-bounce.love', 'binaries/love-0.10.2-win64/logo-bounce.love')
+with cd('binaries/love-0.10.2-win64'):
     subprocess.call('copy /b love.exe+logo-bounce.love logo-bounce.exe', shell=True)
-os.remove('binaries/love-0.10.1-win64/changes.txt')
-os.remove('binaries/love-0.10.1-win64/game.ico')
-os.remove('binaries/love-0.10.1-win64/logo-bounce.love')
-os.remove('binaries/love-0.10.1-win64/love.exe')
-os.remove('binaries/love-0.10.1-win64/love.ico')
-os.remove('binaries/love-0.10.1-win64/readme.txt')
-os.rename('binaries/love-0.10.1-win64', 'binaries/logo-bounce-win64')
+os.remove('binaries/love-0.10.2-win64/changes.txt')
+os.remove('binaries/love-0.10.2-win64/game.ico')
+os.remove('binaries/love-0.10.2-win64/logo-bounce.love')
+os.remove('binaries/love-0.10.2-win64/love.exe')
+os.remove('binaries/love-0.10.2-win64/love.ico')
+os.remove('binaries/love-0.10.2-win64/readme.txt')
+os.rename('binaries/love-0.10.2-win64', 'binaries/logo-bounce-win64')
 
 # upload to itch
 print()
